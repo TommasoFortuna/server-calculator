75fd58dd1f7a24154f4a49acadcba65238722012
lai2-24 <lai2-24@itismeucci.com>
2025-10-15 09:33:46 +0200

    Multi-Thread

--- src/main/java/com/fortuna/Main.java
+++ src/main/java/com/fortuna/Main.java
@@ -1,8 +1,5 @@
 package com.fortuna;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,59 +8,12 @@ public class Main {
         System.out.println("Server avviato");
 
         ServerSocket mioServerSocket = new ServerSocket(3000);
-        Socket mioSocket = mioServerSocket.accept();
-        System.out.println("Qualcuno si è collegato");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
-        PrintWriter out = new PrintWriter(mioSocket.getOutputStream(), true);
-
-        out.println("Versione server: 1.2");
-        System.out.println(in.readLine());
-
-        int op;
         do {
-            int n1 = Integer.parseInt(in.readLine());
-            int n2 = Integer.parseInt(in.readLine());
-            op = Integer.parseInt(in.readLine());
-
-            if (op == 0) {
-                out.println("Connection closed");
-                break;
-
-            }
-
-            float ris;
-
-            switch (op) {
-                case 1:
-                    ris = n1 + n2;
-                    break;
-
-                case 2:
-                    ris = n1 - n2;
-                    break;
-
-                case 3:
-                    ris = (float) n1 / n2;
-                    break;
-
-                case 4:
-                    ris = n1 * n2;
-                    break;
-
-                default:
-                    ris = -1;
-                    break;
-            }
-
-            if (ris == -1)
-                out.println("error 104");
-            else
-                out.println(ris);
-
+            Socket mioSocket = mioServerSocket.accept();
+            MioThread t = new MioThread(mioSocket);
+            t.start();
         } while (true);
-
-        mioServerSocket.close();
 
     }
 }
